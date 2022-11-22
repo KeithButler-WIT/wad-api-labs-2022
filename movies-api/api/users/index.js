10a515de4a56475b73ed3b7b4721abffdd56b78d
Keith Butler <20089137@mail.wit.ie>
2022-11-22 19:01:55 +0000

    Exercise - Password validation format

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -19,8 +19,12 @@ router.post('/',asyncHandler( async (req, res, next) => {
       return next();
     }
     if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
+      if (req.body.password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/g)) {
+        await User.create(req.body);
+        res.status(201).json({code: 201, msg: 'Successful created new user.'});
+      } else {
+        res.status(401).json({code: 401,msg: 'User creation failed.'});
+      }
     } else {
       const user = await User.findByUserName(req.body.username);
         if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
