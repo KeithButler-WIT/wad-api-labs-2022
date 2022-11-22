ad2d4bb99bac55dcdbb240e11e957466e8695394
Keith Butler <20089137@mail.wit.ie>
2022-11-22 18:07:00 +0000

    Topic 10 - Passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,7 +3,7 @@ import './seedData'
 import dotenv from 'dotenv';
 import express from 'express';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import usersRouter from './api/users';
@@ -23,15 +23,10 @@ const app = express();
 
 const port = process.env.PORT;
 
-//session middleware
-app.use(session({
-  secret: 'ilikecake',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(passport.initialize());
 
 app.use(express.json());
-app.use('/api/movies', authenticate, moviesRouter);
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
